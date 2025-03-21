6e9795487a12b46e87328f1a487ea9b6b9a20894
Stonoly <pierre.moly@gmail.com>
2025-03-21 11:06:12 +0100

    TP4 - Exercice 1

---
+++ tp4/exercice2.py
@@ -0,0 +1,14 @@
+# Question 1. Donnez, sous forme d’une équation de récurrence, le temps d’exécution T(n) de cet algorithme 
+#             en fonction du nombre n d’éléments dans le tableau à trier.
+def sort_by_fusion(list: list) -> list:
+    return list
+
+if __name__ == "__main__":
+
+
+    list_of_test = [64, 34, 25, 12, 22, 11, 90, 45, 78, 3]
+
+
+    print("Liste originale:", list_of_test)
+    sorted_list = sort_by_fusion(list_of_test)
+    print("Liste triée:", sorted_list)
